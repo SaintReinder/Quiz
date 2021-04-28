e9186eb17db10ece60b97afcc78bc68a3600f790
Daniel <daniel.miranda.t@mail.pucv.cl>
2021-04-28 05:08:13 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PrincipalComponent} from '../app/pages/principal/principal.component';
+import { ListaComponent } from '../app/pages/lista/lista.component';
+import { AdicionarComponent } from '../app/pages/adicionar/adicionar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'vistacard', component: PrincipalComponent},
+  {path: 'lista', component: ListaComponent},
+  {path: 'adicionar', component: AdicionarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatButtonModule} from '@angular/material/button';
+import { ListaComponent } from './pages/lista/lista.component';
+import { AdicionarComponent } from './pages/adicionar/adicionar.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PrincipalComponent,
+    ListaComponent,
+    AdicionarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
